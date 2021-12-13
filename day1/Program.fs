e0dc97068dc971e24da31a0ae4ac90fa8899a5a3
breno.afb <breno.afb@gmail.com>
2021-12-12 22:57:45 -0300

    Add day 1 solution

---
+++ day1/Program.fs
@@ -0,0 +1,30 @@
+﻿let solution xs =
+    let tail = List.tail xs
+    let init = List.removeAt (List.length xs - 1) xs
+    List.zip init tail
+    |> List.map (fun (x,y) -> (y - x))
+    |> List.filter (fun x -> x > 0)
+    |> List.length
+
+let solution' xs =
+    let tail = List.tail xs
+               |> List.tail
+    let mid = List.removeAt (List.length xs - 1) xs
+              |> List.tail
+    let head = List.removeAt (List.length xs - 1) xs
+               |> List.removeAt (List.length xs - 2)
+    List.zip3 tail mid head
+    |> List.map (fun (x,y,z) -> (x + y + z))
+    |> solution
+
+let readIntList filename =
+    System.IO.File.ReadLines(filename)
+    |> Seq.toList
+    |> List.map(fun x -> int x)
+
+
+let input = readIntList "input.txt"
+let result = solution input
+let result' = solution' input
+printfn "result: %d" result
+printfn "result': %d" result'
